8d7b142407489ce191bda9168f95511bde3e6ef9
Piotr Sarna <sarna@chiselstrike.com>
2023-01-09 16:22:22 +0100

    Dockerfile: switch to IPv6 for Postgres by default
    The value can be overridden with SQLD_PG_ADDR environment variable.
    Setting the default to IPv6's localhost makes it possible to run
    sqld on fly.io on free tier, which was not the case with public ipv4.

--- USER_GUIDE.md
+++ USER_GUIDE.md
@@ -10,9 +10,9 @@ flyctl launch
 ```
 ... then pick a name and respond "Yes" when the prompt asks you to deploy.
 
-Finaly, allocate a IPv4 addres:
+Finally, allocate an IPv6 addres:
 ```
-flyctl ips allocate-v4 -a <your app name>
+flyctl ips allocate-v6 -a <your app name>
 ```
 
 You now have `sqld` running on Fly listening to port `5000`.
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,13 +3,14 @@
 set -Eeuo pipefail
 
 SQLD_NODE="${SQLD_NODE:-primary}"
+SQLD_PG_ADDR="${SQLD_PG_ADDR:-[::]:5000}"
 
 if [ "$1" = '/bin/sqld' ]; then
   # We are running the server.
   declare -a server_args=()
 
   # Listen to PostgreSQL port by default.
-  server_args+=("--pg-listen-addr" "0.0.0.0:5000")
+  server_args+=("--pg-listen-addr" "$SQLD_PG_ADDR")
 
   # Set remaining arguments depending on what type of node we are.
   case "$SQLD_NODE" in
